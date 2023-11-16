1d5903c4c775a2558aa0bb6f533d1c8eaa88340b
donovantran <146876081+donovantran@users.noreply.github.com>
2023-11-16 11:58:54 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace1(){
+    int[] input1 = {5,4,3,2,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1,2,3,4,5},input1);
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+public void testReversed1(){
+  int[] input1 = {1,2,3,4};
+  assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input1));
+}
 }
