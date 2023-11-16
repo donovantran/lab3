d748e8dbb9127983eeb9d305b8b3485e5fa44487
donovantran <146876081+donovantran@users.noreply.github.com>
2023-11-16 12:02:53 -0800

    Update ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -3,6 +3,7 @@
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
+  static void reverseInPlace(int[] arr) {
     for(int i = 0; i < arr.length/2; i++) {
       int holder;
       holder = arr[i];
